98bf7e51acc1c40f6771e7da80be5bfe1b6f7b6d
lspindler2509 <lisa.spindler@hotmail.de>
2024-03-19 17:37:15 +0100

    added test script for db

---
+++ nedrexdb/db/parsers/db_test.py
@@ -0,0 +1,18 @@
+import pymongo
+from configparser import ConfigParser
+
+
+
+# Holen Sie sich die MongoDB-Konfiguration für die Entwicklungsdatenbank
+mongo_port = 27019
+mongo_db_name = "licensed_nedrex_dev"
+
+# Verbinde dich mit der MongoDB-Datenbank
+client = pymongo.MongoClient('localhost', mongo_port)
+db = client[mongo_db_name]
+
+# Beispielabfrage
+collection = db['GeneAssociatedWithDisorder']
+num_documents = collection.count_documents({})
+
+print("Anzahl der Dokumente:", num_documents)
