ac425495297f2f618f6c403578612d354d204218
lspindler2509 <lisa.spindler@hotmail.de>
2024-05-21 10:04:22 +0200

    adjusted edgetype

--- nedrexdb/db/parsers/cosmic.py
+++ nedrexdb/db/parsers/cosmic.py
@@ -212,7 +212,7 @@ class COSMICParser:
                         gene_disorder = variant_gene.copy()
                         gene_disorder.sourceDomainId = variant_gene.targetDomainId
                         gene_disorder.targetDomainId = variant_disorder.targetDomainId
-                        gene_disorder_updates.append(gene_disorder.generate_update())
+                        gene_disorder_updates.append(GeneAssociatedWithDisorder(gene_disorder).generate_update())
 
             for this_collection_name, these_updates in zip([GenomicVariant.collection_name, VariantAffectsGene.collection_name, VariantAssociatedWithDisorder.collection_name, GeneAssociatedWithDisorder.collection_name],
                                                            [genomic_variant_updates, variant_gene_updates, variant_disorder_updates, gene_disorder_updates]):
